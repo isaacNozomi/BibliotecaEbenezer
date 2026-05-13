52618e959d6b4e84bc46608a34a130beff33f363
isaacNozomi <isaacrodriguezcentella@gmail.com>
2026-05-13 09:57:38 -0400

    "mejora 3"

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,5 @@
 plugins {
     id("com.android.application") version "8.2.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
-    // Kapt se usa en el módulo sin necesidad de una versión aquí,
-    // pero debemos declararlo para que el módulo pueda usarlo.
     id("org.jetbrains.kotlin.kapt") version "1.9.22" apply false
 }
